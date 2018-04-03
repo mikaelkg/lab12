65f21e46b546f7edda93b63bec252c513118583b
mikaelkg <5019973@mail.ru>
2018-04-03 20:51:23 +0300

    refactoring

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char** argv) {
 std::string text;
 std::string log_path = std::getenv("LOG_PATH");
-std::ofstream out(log_path:ios_base::app);
+std::ofstream out(log_path, std::ios_base::app);
 while(std::cin >> text) {
 print(text, out);
 out << std::endl;
